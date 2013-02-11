1ed090e5a0e4b277034ced3837f057a932721366
Troy Conrad Therrien <troyth@gmail.com>
2013-02-11 08:27:25 -0500

    update

--- 2013/group2/step1/js/script.js
+++ 2013/group2/step1/js/script.js
@@ -37,34 +37,18 @@ $(document).ready(function() {
 	 *	
 	 *	A function that replaces the src attribute of the #image img in the index.html file
 	 *
+	 *	@todo: implement zoomOut using zoomIn as a model
+	 *
 	*/
 	function zoomOut(){
 		console.log('zoomOut() called');//log to the console each time this function is called
-
-		//get the index of the current image assuming each image will be named cat#.jpg
-		var idx = $('#image img').attr('src');
-		idx = idx.substr(7,1);
-		idx = parseInt(idx);
-
-		//only decrement if greater than 1
-		if(idx > 1){
-			//decrement the index
-			idx--;
-			console.log('new idx: '+ idx);//print out the new index
-		}
-
-		//construct the new source string
-		var src = 'img/cat' + idx + '.jpg';
-
-		//swap the image for the next
-		$('#image img').attr('src', src);
 	}
 
 	//bind the #zoom-in anchor to the zoomIn() function on click
 	$('#zoom-in').bind('click', zoomIn);
 
 	//bind the #zoom-out anchor to the zoomOut() function on click
-	$('#zoom-out').bind('click', zoomOut);
+	//@todo: your code here to bind zoomOut to the #zoom-out anchor
 
 
 });
