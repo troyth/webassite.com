eb7d5b7a242160458d6727c20c5b98236497ec97
Troy Conrad Therrien <troyth@gmail.com>
2013-02-28 09:42:53 -0500

    added functionality for streaming and a timeout for the entire server after 12 hours

--- util/twitter-api/REST/server.js
+++ util/twitter-api/REST/server.js
@@ -25,18 +25,26 @@ app.configure(function () {
 app.get('/tweets/geo/bowery', tweets.findAll);
 app.get('/tweets/geo/bowery/limit/:limit', tweets.findAllLimited);
 
-app.get('/tweets/id/:id', tweets.findById);
+app.get('/tweets/geo/bowery/id/:id', tweets.findById);
 app.get('/tweets/geo/bowery/hashtag/:hashtag', tweets.findByHashtag);
 app.get('/tweets/geo/bowery/hashtag/:hashtag/limit/:limit', tweets.findByHashtagLimited);
+
+
+app.get('/tweets/stream/bowery', tweets.findAllStream);
+
+
 
 app.listen(3000);
 
 tweets.stream();
+tweets.countStream();
 
 var interval_id = setInterval(function(){ tweets.fetch(); }, 20000);//fetch every 20 seconds
 
 //tweets.fetch();
 //console.log('interval_id: ');
 //console.dir(interval_id);
+
+setTimeout(function(){ process.exit(); }, 43200000);//14400000 = 4hrs, 43200000 = 12hrs
 
 console.log('Listening on port 3000...');
